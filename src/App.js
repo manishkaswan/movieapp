643349159e103a09bc0501099369417fb8e32cb6
manish kumar <manishkaswan5@gmail.com>
2024-03-20 20:36:36 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 import axios from 'axios';
@@ -6,31 +6,50 @@ import axios from 'axios';
 function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const retryTimeoutRef = useRef(null);
 
-  
-  const fetchMovies = () => {
+  const fetchMovies = async () => {
     setLoading(true);
-    axios.get('https://swapi.dev/api/people')
-      .then((res) => {
+    try {
+      const res = await axios.get('https://swapi.dev/api/people');
+      if (res.status === 200) {
         setMovies(res.data.results);
         setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching movies:', error);
-      });
+        setError(null);
+      } else {
+        throw new Error('Failed to fetch data');
+      }
+    } catch (error) {
+      console.error('Something went wrong ....Retrying');
+      setLoading(false);
+      setError(error);
+      retryFetch();
+    }
   };
-  useEffect(() => {
-    fetchMovies();
-  },[])
+
+  const retryFetch = () => {
+    retryTimeoutRef.current = setTimeout(fetchMovies, 5000);
+  };
+
+  const cancelRetry = () => {
+    if (retryTimeoutRef.current) {
+      clearTimeout(retryTimeoutRef.current);
+    }
+  };
+
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
+        <button onClick={cancelRetry}>Cancel Retry</button>
       </section>
       <section>
-        {!loading && <MoviesList movies={movies} />}
         {loading && <p>Loading...</p>}
+        {!loading && movies.length === 0 && <p>No movies found</p>}
+        {error && <p>Error: {error.message}</p>}
+        {!loading && !error && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
